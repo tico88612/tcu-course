8ea51146cfa1a0d6a790eef6a5193b2091d084f2
tico88612 <tico88612@yahoo.com.tw>
2019-09-08 14:24:13 +0800

    add Icon & remove license

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    resizable: false
+    resizable: false,
+    icon: path.join(__dirname, 'icon/favicon.png')
   })
 
   // and load the index.html of the app.
