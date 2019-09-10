561a77b38745536bece72db25ad0052ded104e21
tico88612 <tico88612@yahoo.com.tw>
2019-09-10 19:51:47 +0800

    Add login function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const log = require('electron-log');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -31,6 +32,7 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+  mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
@@ -55,21 +57,154 @@ app.on('activate', function () {
 // code. You can also put them in separate files and require them here.
 
 const { ipcMain } = require('electron')
+const rp = require('request-promise')
+const querystring = require('querystring')
 
+let __EVENTTARGET, __EVENTARGUMENT, __VIEWSTATE, __VIEWSTATEENCRYPTED, __EVENTVALIDATION
 let userId, userPwd;
+let userCookie;
 
 ipcMain.on('login-user-function', startSession)
 
-function startSession(event, arg1, arg2) {
-  userId = arg1;
-  userPwd = arg2;
+async function startSession(event, arg1, arg2) {
+  log.info("startSession Function START");
+
+  userId = arg1
+  userPwd = arg2
+  log.debug("userId: %s", userId);
+  log.debug("userPwd: %s", userPwd);
+
+  log.info("GET Choose Course System Data START");
+  let htmlResponse = await useRequestPromise({
+    uri: "http://203.64.78.174/ScasWebSite/Default.aspx",
+    timeout: 30000,
+    followRedirect: true,
+    maxRedirects: 10,
+    simple: false
+  });
+  curlParseASPX(htmlResponse)
+  log.info("GET Choose Course System Data END");
+  
+  log.info("Make POST Body Data START");
+  let form = {
+    "__EVENTTARGET": __EVENTTARGET,
+    "__EVENTARGUMENT": __EVENTARGUMENT,
+    "__VIEWSTATE": __VIEWSTATE,
+    "__EVENTVALIDATION": __EVENTVALIDATION,
+    "logUser$UserName": userId,
+    "logUser$Password": userPwd,
+    "logUser$LoginButton": "登　入"
+  }
+  let formData = querystring.stringify(form);
+  let formDataLength = formData.length;
+  log.debug("FormData: %s", formData);
+  log.debug("FormData - length: %d", formDataLength);
+  log.info("Make POST Body Data END");
+
+  log.info("POST Choose Course System Data START");
+  htmlResponse = await useRequestPromise({
+    headers: {
+      'Cookie': userCookie,
+      'Content-length': formDataLength,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    uri: "http://203.64.78.174/ScasWebSite/Default.aspx",
+    method: "POST",
+    body: formData,
+    timeout: 30000,
+    resolveWithFullResponse: true,
+    maxRedirects: 10,
+    simple: false
+  });
+
+  if (htmlResponse.statusCode === 302 && htmlResponse.body.indexOf('SelectAddCode.aspx') > -1){
+    log.info("Login Successful!")
+    event.sender.send('login-Success-Alert')
+  }
+  else{
+    log.info("Login Failure!")
+    event.sender.send('login-Fail-Alert')
+  }
+
+  log.info("startSession Function END");
 }
 
-ipcMain.on('add-count', (event, arg) => {
-  count++
-  mainWindow.webContents.send('res-count', count)
-})
+function curlParseASPX(body){
+  log.info("curlParseASPX Function START");
+  
+  let arr;
+  body = body.replace(/(\r\n|\n|\r|\s)/gm, '')
+  arr = body.toString().split("id=\"__EVENTTARGET\"value=\"")
+  if (arr.length >= 2){
+    arr = arr[1];
+    arr = arr.toString().split("\"/>");
+    __EVENTTARGET = arr[0];
+  }
+  else{
+    __EVENTTARGET = "";
+  }
+  log.debug("__EVENTTARGET: %s", __EVENTTARGET);
 
-ipcMain.on('req-count', (event, arg) => {
-  event.sender.send('res-count', count)
-})
+  arr = body.toString().split("id=\"__EVENTARGUMENT\"value=\"")
+  if (arr.length >= 2) {
+    arr = arr[1];
+    arr = arr.toString().split("\"/>");
+    __EVENTARGUMENT = arr[0];
+  }
+  else{
+    __EVENTARGUMENT = "";
+  }
+  log.debug("__EVENTARGUMENT: %s", __EVENTARGUMENT);
+
+  arr = body.toString().split("id=\"__VIEWSTATE\"value=\"")
+  if (arr.length >= 2) {
+    arr = arr[1];
+    arr = arr.toString().split("\"/>");
+    __VIEWSTATE = arr[0];
+  }
+  else{
+    __VIEWSTATE = "";
+  }
+  log.debug("__VIEWSTATE: %s", __VIEWSTATE);
+
+
+  arr = body.toString().split("id=\"__VIEWSTATEENCRYPTED\"value=\"")
+  if (arr.length >= 2) {
+    arr = arr[1];
+    arr = arr.toString().split("\"/>");
+    __VIEWSTATEENCRYPTED = arr[0];
+  }
+  else {
+    __VIEWSTATEENCRYPTED = "";
+  }
+  log.debug("__VIEWSTATEENCRYPTED: %s", __VIEWSTATEENCRYPTED);
+
+
+  arr = body.toString().split("id=\"__EVENTVALIDATION\"value=\"")
+  if (arr.length >= 2) {
+    arr = arr[1];
+    arr = arr.toString().split("\"/>");
+    __EVENTVALIDATION = arr[0];
+  }
+  else {
+    __EVENTVALIDATION = "";
+  }
+  log.debug("__EVENTVALIDATION: %s", __EVENTVALIDATION);
+
+  log.info("curlParseASPX Function END");
+}
+
+async function useRequestPromise(options) {
+  let rpbody = await rp(options, function (error, response, body) {
+    if (error !== null) {
+      log.error(error)
+    }
+    else {
+      if (response.headers["set-cookie"]) {
+        userCookie = response.headers["set-cookie"][0]
+      }
+    }
+
+  });
+  return rpbody;
+}
--- renderer.js
+++ renderer.js
@@ -5,6 +5,19 @@ function updateCount(event, arg) {
 function loginUser() {
     let userId = document.getElementById('studentId').value
     let userPwd = document.getElementById('studentPwd').value
+    document.getElementById('loginBtn').classList.add("is-loading");
     ipcRenderer.send('login-user-function', userId, userPwd)
 }
-ipcRenderer.on('res-count', updateCount)
+function loginFail(params) {
+    alert('帳號密碼錯誤');
+    document.getElementById('loginBtn').classList.remove("is-loading");
+}
+function loginSuccess(params) {
+    document.getElementById('loginBtn').classList.remove("is-loading");
+    document.getElementById('loginBtn').classList.remove("is-info");
+    document.getElementById('loginBtn').classList.add("is-success");
+    document.getElementById('loginBtn').innerHTML = "登入成功";
+    document.getElementById('loginBtn').disabled = true;
+}
+ipcRenderer.on('login-Fail-Alert', loginFail)
+ipcRenderer.on('login-Success-Alert', loginSuccess)
